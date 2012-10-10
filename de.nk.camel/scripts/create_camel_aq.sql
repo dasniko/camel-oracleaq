5dba3a62f47951f7a6c3df26fbacd1c36c9a88f1
Niko Köbler <niko@n-k.de>
2012-10-10 15:45:02 +0200

    commented out unused ora user/roles/privileges

--- de.nk.camel/scripts/create_camel_aq.sql
+++ de.nk.camel/scripts/create_camel_aq.sql
@@ -1,3 +1,4 @@
+/*
 -- role for AQ admin user
 create role camel_aq_admin_role;
 grant connect, resource, aq_administrator_role, create any procedure to camel_aq_admin_role;
@@ -22,6 +23,7 @@ create user camel_aq_user identified by camelaq;
 grant connect, resource, create session to camel_aq_user;
 grant camel_aq_user_role to camel_aq_user;
 grant execute on dbms_aq to camel_aq_user;
+*/
 
 -- create queue(s)
 exec dbms_aqadm.create_queue_table(queue_table => 'CAMEL_IN_T', queue_payload_type => 'SYS.AQ$_JMS_TEXT_MESSAGE', multiple_consumers => false);
@@ -32,16 +34,17 @@ exec dbms_aqadm.create_queue_table(queue_table => 'CAMEL_OUT_T', queue_payload_t
 exec dbms_aqadm.create_queue(queue_name => 'CAMEL_OUT', queue_table => 'CAMEL_OUT_T');
 exec dbms_aqadm.start_queue(queue_name => 'CAMEL_OUT');
 
+/*
 -- grant the camel_aq_user_role the needed rights to inserts and modify
 grant select, delete, insert, update on camel_in_t to camel_aq_user_role;
 grant select, delete, insert, update on camel_out_t to camel_aq_user_role;
-
+*/
 commit;
 
-create table CAMEL_DEMO (
-  "ID" NUMBER,
-  MESSAGE VARCHAR2(80 BYTE),
-  DATETIME DATE
+create table camel_demo (
+  id number,
+  message varchar2(80 byte),
+  datetime date
   );
-  
+
 commit;
--- de.nk.camel/scripts/drop_camel_aq.sql
+++ de.nk.camel/scripts/drop_camel_aq.sql
@@ -1,7 +1,9 @@
+/*
 drop role camel_aq_admin_role;
 drop role camel_aq_user_role;
 drop user camel_aq_admin;
 drop user camel_aq_user;
+*/
 
 exec dbms_aqadm.stop_queue(queue_name => 'CAMEL_IN');
 exec dbms_aqadm.drop_queue(queue_name => 'CAMEL_IN');
@@ -13,5 +15,5 @@ exec dbms_aqadm.drop_queue_table(queue_table => 'CAMEL_OUT_T');
 
 commit;
 
-drop table CAMEL_DEMO;
+drop table camel_demo;
 commit;
